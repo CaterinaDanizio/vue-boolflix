b229db8ac84fa9152371d33cd7d0fde85005e876
Caterina <danizio@outlook.it>
2020-12-03 14:04:07 +0100

    Rating film e serie

--- js/script.js
+++ js/script.js
@@ -1,17 +1,32 @@
-const database = "https://api.themoviedb.org/3/search/tv?api_key=0b739e8a0353e6df48b40dbc54ebd08e&query="
+const databaseMovie = "https://api.themoviedb.org/3/search/movie?api_key=0b739e8a0353e6df48b40dbc54ebd08e&query="
+
+const databaseTv = "https://api.themoviedb.org/3/search/tv?api_key=0b739e8a0353e6df48b40dbc54ebd08e&query="
+
 var app = new Vue({
   el: '#app',
   data: {
     userInput: "",
-    media: []
+    mediaMovie: [],
+    mediaTv: []
   },
 
+// Funzione che contiene chiamata API collegata all'inserimento di una query di ricerca nell'input
   methods: {
     search: function () {
-      axios.get(database + this.userInput).then(risposta => {
+      axios.get(databaseMovie + this.userInput).then(risposta => {
         let movies = risposta.data.results;
-        this.media = movies;
-      })
+        this.mediaMovie = movies;
+      });
+
+// Chiamata API per le serie TV
+      axios.get(databaseTv + this.userInput).then(risposta => {
+        let serieTv = risposta.data.results;
+        this.mediaTv = serieTv;
+      });
     },
+     rating: function (vote) {
+             return Math.round(vote / 2);
+           }
   }
+
 });
