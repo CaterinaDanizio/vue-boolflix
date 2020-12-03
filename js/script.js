19c75ea9c1dab810060d234ac9da99aabb788133
Caterina <danizio@outlook.it>
2020-12-03 21:35:42 +0100

    Milestone 2-3 - stampa locandina, star rating e flag lingua

--- js/script.js
+++ js/script.js
@@ -2,12 +2,20 @@ const databaseMovie = "https://api.themoviedb.org/3/search/movie?api_key=0b739e8
 
 const databaseTv = "https://api.themoviedb.org/3/search/tv?api_key=0b739e8a0353e6df48b40dbc54ebd08e&query="
 
+const apikey = "0b739e8a0353e6df48b40dbc54ebd08e"
+
 var app = new Vue({
   el: '#app',
   data: {
     userInput: "",
     mediaMovie: [],
-    mediaTv: []
+    mediaTv: [],
+    locandina: "https://image.tmdb.org/t/p/w185",
+    flag: "https://www.countryflags.io/",
+    spec: "/flat/24.png",
+    en: "https://www.countryflags.io/gb/flat/24.png",
+    ja: "https://www.countryflags.io/jp/flat/24.png",
+
   },
 
 // Funzione che contiene chiamata API collegata all'inserimento di una query di ricerca nell'input
@@ -17,13 +25,18 @@ var app = new Vue({
         let movies = risposta.data.results;
         this.mediaMovie = movies;
       });
+      this.userInput = "";
+
 
 // Chiamata API per le serie TV
       axios.get(databaseTv + this.userInput).then(risposta => {
         let serieTv = risposta.data.results;
         this.mediaTv = serieTv;
       });
+      this.userInput = "";
     },
+
+// Funzione per portare il voto in un range da 1 a 5 e arrotondarlo per eccesso
      rating: function (vote) {
              return Math.round(vote / 2);
            }
